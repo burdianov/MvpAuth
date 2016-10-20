0d1def80a0851ac7f1d5ef1ac8b3ac55b4913b0c
Leo Burdianov <leo.burdianov@gmail.com>
2016-10-20 22:27:59 +0400

    Initial commit

---
+++ app/src/main/java/com/testography/mvpauth/ui/activities/RootActivity.java
@@ -0,0 +1,16 @@
+package com.testography.mvpauth.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.testography.mvpauth.R;
+
+public class RootActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(
+                savedInstanceState);
+        setContentView(R.layout.activity_root);
+    }
+}
